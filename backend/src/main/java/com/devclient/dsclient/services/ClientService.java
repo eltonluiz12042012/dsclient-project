f32c8536110319901440b4e8b4416193535ef450
Elton Luiz <eltonluiz12042012@gmail.com>
2023-01-05 18:31:14 -0300

    DTO, entity, repository e services

---
+++ backend/src/main/java/com/devclient/dsclient/services/ClientService.java
@@ -0,0 +1,50 @@
+package com.devclient.dsclient.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devclient.dsclient.dto.ClientDTO;
+import com.devclient.dsclient.entities.Client;
+import com.devclient.dsclient.repositories.ClientRepository;
+import com.devclient.dsclient.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly= true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequets){
+		Page<Client> list = repository.findAll(pageRequets);
+		return list.map(x-> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly= true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(()-> new ResourceNotFoundException("Recurso Inexistente"));
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		DtoToEntity(entity, dto);
+		return new ClientDTO(repository.save(entity));
+	}
+	
+	private void DtoToEntity(Client client, ClientDTO dto) {
+		client.setName(dto.getName());
+		client.setIncome(dto.getIncome());
+		client.setCpf(dto.getCpf());
+		client.setChildren(dto.getChildren());
+		client.setBirthDate(dto.getBirthDate());
+		
+	}
+}
---
+++ backend/src/main/java/com/devclient/dsclient/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.devclient.dsclient.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
