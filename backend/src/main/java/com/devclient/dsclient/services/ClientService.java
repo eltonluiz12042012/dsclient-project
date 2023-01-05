3a70dc4706d419a4b340e5e0132482156a98dac7
Elton Luiz <eltonluiz12042012@gmail.com>
2023-01-05 19:28:01 -0300

    Resource client

--- backend/src/main/java/com/devclient/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devclient/dsclient/services/ClientService.java
@@ -2,7 +2,11 @@ package com.devclient.dsclient.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devclient.dsclient.dto.ClientDTO;
 import com.devclient.dsclient.entities.Client;
 import com.devclient.dsclient.repositories.ClientRepository;
+import com.devclient.dsclient.services.exceptions.DatabaseException;
 import com.devclient.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -37,6 +42,27 @@ public class ClientService {
 		Client entity = new Client();
 		DtoToEntity(entity, dto);
 		return new ClientDTO(repository.save(entity));
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			DtoToEntity(entity, dto);
+			return new ClientDTO(repository.save(entity));
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("ID inexistente " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("ID not found: " + id);
+		}catch(DataIntegrityViolationException e) {
+			throw new ResourceNotFoundException("Integrity violation");
+		}
 	}
 	
 	private void DtoToEntity(Client client, ClientDTO dto) {
